dd515552558d5e1e58d19010577ba1232ce5dfd4
Sean <seanha2013@gmail.com>
2020-11-23 23:58:16 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private float jumpForce = 7;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        else if(Input.GetKeyUp(KeyCode.Space) && rb.velocity.y > 0)
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(0, jumpForce);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# PekoraGame
+ A small endless runner game featuring Usada Pekora from Hololive
